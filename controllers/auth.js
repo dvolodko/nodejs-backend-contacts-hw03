3b0199e4d371fca42708f104241bfbe2a0946482
Denys Volodko <babyleejane@gmail.com>
2023-12-07 20:47:36 +0200

    auth middleware added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,11 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const { User } = require('../models/user');
 
 const { HttpError, ctrlWrapper } = require('../helpers');
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -32,7 +35,11 @@ const login = async (req, res) => {
     throw HttpError(401, 'Email or password invalid');
   }
 
-  const token = 'lkjsdfl.23f2f3wewsdf.2r2fesdf23fwef2fe';
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
 
   res.json({ token });
 };
